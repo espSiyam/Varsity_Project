973592a958bc918c173d1d298ed67764caee0d0a
espsiyam <sohag35-292@diu.edu.bd>
2023-09-09 18:58:32 +0300

    proper resposes

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from xml.etree import ElementTree as ET
 import httpx
 
 app = FastAPI()
 
 # Configure Jinja2 templates
 templates = Jinja2Templates(directory="templates")
+
+# Custom Jinja2 filters
+def parse_xml(value):
+    return ET.fromstring(value)
+
+def extract_result(xml_element):
+    result_element = xml_element.find(".//AddResult")
+    if result_element is not None:
+        return result_element.text
+    else:
+        return "Result not found in SOAP response"
+
+templates.env.filters["parse_xml"] = parse_xml
+templates.env.filters["extract_result"] = extract_result
 
 # Define an endpoint to render the HTML form
 @app.get("/")
@@ -14,7 +29,7 @@ async def index(request: Request):
 
 # Define an endpoint to process the form submission
 @app.post("/calculate")
-async def calculate(request: Request, intA: int, intB: int):
+async def calculate(request: Request, intA: int = Form(...), intB: int = Form(...)):
     # Define the SOAP request XML
     soap_request = f"""<?xml version="1.0" encoding="utf-8"?>
     <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
@@ -37,10 +52,14 @@ async def calculate(request: Request, intA: int, intB: int):
     
     # Check if the request was successful
     if response.status_code == 200:
-        # Extract and return the result from the SOAP response
+        # Extract and return the SOAP response as is
         soap_response = response.text
     else:
         # Handle errors here
         soap_response = "SOAP request failed"
 
     return templates.TemplateResponse("result.html", {"request": request, "soap_response": soap_response})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
