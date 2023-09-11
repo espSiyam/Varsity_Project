a62cf365459a21b5a20c96ae52769064b3e38cf4
espsiyam <sohag35-292@diu.edu.bd>
2023-09-11 13:10:16 +0300

    cleaning and requirements

--- main.py
+++ main.py
@@ -1,66 +1,53 @@
+# Importing the libraries
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 import httpx
 import xml.etree.ElementTree as ET
 import re
 
+# Basic FastAPI app and Jinja Template setup
 app = FastAPI()
-
-# Configure Jinja2 templates
 templates = Jinja2Templates(directory="templates")
 
-# Define an endpoint to render the HTML form
-@app.get("/")
+# Endpoint to render the input form
+# @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-# Define an endpoint to process the form submission
+# Endpoint to make SOAP request and render the response
 @app.post("/calculate")
 async def calculate(request: Request, intA: int = Form(...), intB: int = Form(...)):
-    # Define the SOAP request XML
+
     soap_request = f"""<?xml version="1.0" encoding="utf-8"?>
     <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
         <soap:Body>
             <tem:Multiply>
-                <tem:intA>{intA}</tem:intA>
-                <tem:intB>{intB}</tem:intB>
+                <tem:intA>{intA}</tem:intA> # First input for the multiplication
+                <tem:intB>{intB}</tem:intB> # Second input for the multiplication
             </tem:Multiply>
         </soap:Body>
     </soap:Envelope>
     """
     
-    async with httpx.AsyncClient() as client:
-        # Make a POST request to the SOAP service endpoint
+    async with httpx.AsyncClient() as client: # To make the request to the SOAP web service.
         response = await client.post(
             "http://www.dneonline.com/calculator.asmx",
             data=soap_request,
             headers={"Content-Type": "text/xml; charset=utf-8"},
         )
     
-    # Check if the request was successful
+    # Handling the response from the SOAP web service
     if response.status_code == 200:
-        # Extract and return the result from the SOAP response
         soap_response = response.text
     else:
-        # Handle errors here
         soap_response = "SOAP request failed"
-    print(soap_response)
 
-    # Remove the XML declaration
+    # Filtering the response xml to get the result
     cleaned_xml = re.sub(r'<\?xml.*\?>', '', soap_response)
-
-    # Remove namespaces from elements
     cleaned_xml = re.sub(r'xmlns="[^"]+"', '', cleaned_xml)
-
-    # Parse the cleaned XML
     root = ET.fromstring(cleaned_xml)
-
-    # Find the MultiplyResult element and get its text content
     multiply_result_element = root.find(".//MultiplyResult")
-    if multiply_result_element is not None:
-        multiply_result_text = multiply_result_element.text
-        print("Value extracted from <MultiplyResult>:", multiply_result_text)
-    else:
-        print("MultiplyResult element not found in the XML")
+    multiply_result_text = multiply_result_element.text
 
+    # Rendering the response template with the result
     return templates.TemplateResponse("result.html", {"request": request, "soap_response": multiply_result_text})
---
+++ versions.py
@@ -0,0 +1,9 @@
+import fastapi
+import httpx
+import xml.etree.ElementTree as ET
+import re
+
+print("FastAPI Version:", fastapi.__version__)
+print("httpx Version:", httpx.__version__)
+print("xml.etree.ElementTree Version:", ET.VERSION)
+print("re Version:", re.__version__)
